fb4de383cdf631b752a590bd22ef414f0c92fdc5
lmarquar <leon.marquardt@gmx.de>
2025-12-18 11:09:33 +0100

    fixed styling so it's all readable now!

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,11 +1,9 @@
+import 'package:flutter/material.dart';
+
 class KConstants {
   static const String themeModeKey = 'isDarkKey';
   static const String appName = 'StimmApp';
-}
 
-class KValue {
-  static const String basicLayout = 'Basic Layout';
-  static const String cleanUi = 'clean UI';
-  static const String fixBugs = 'fix bugs';
-  static const String keyConcepts = 'key concepts';
+  static const Color appColor = Colors.greenAccent;
+  static const Color lightColor = Colors.black;
 }
