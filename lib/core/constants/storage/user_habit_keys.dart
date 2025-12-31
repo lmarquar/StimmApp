8be95f47b5373b91ff4ea6b95d81d059feb62bfe
lmarquar <leon.marquardt@gmx.de>
2025-12-31 08:47:00 +0100

    with og change pic

--- lib/core/constants/storage/user_habit_keys.dart
+++
@@ -1,7 +0,0 @@
-class UserHabitKeys {
-  static const String name = 'name';
-  static const String progress = 'progress';
-  static const String count = 'count';
-  static const String lastUpdated = 'last_updated'; //timestamp
-  static const String date = 'date';
-}
