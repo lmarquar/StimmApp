7d46e57c5ceeb319300dca76fbb4efe30030183c
lmarquar <leon.marquardt@gmx.de>
2026-01-08 09:35:06 +0100

    kotlin test call works

--- android/app/src/main/kotlin/com/example/stimmapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/stimmapp/MainActivity.kt
@@ -10,6 +10,7 @@ import android.content.IntentFilter
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.util.Log
+import io.flutter.plugin.common.MethodCall
 
 
 class MainActivity: FlutterActivity() {
@@ -17,19 +18,29 @@ class MainActivity: FlutterActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+        fun processUserName(
+            call: MethodCall,
+            userName: String,
+            result: MethodChannel.Result
+        ) {
+            // Your code here
+            var userName1 = userName
+
+            @Suppress("UNCHECKED_CAST")
+            val arguments = call.arguments as? List<Map<String, Any>> ?: emptyList()
+            userName1 = arguments.firstOrNull()?.get("text") as? String ?: "defaultUser"
+            println("Received userName::: $userName1")
+            val resultMap = mapOf("userName" to userName1)
+            result.success(resultMap)
+        }
+
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel).setMethodCallHandler {
                 call, result ->
             // This method is invoked on the main thread.
             var userName = "defaultUser";
             if (call.method == "passDataToNative") {
-                // Your code here
-                @Suppress("UNCHECKED_CAST")
-                val arguments = call.arguments as? List<Map<String, Any>> ?: emptyList()
-                userName = arguments.firstOrNull()?.get("text") as? String ?: "defaultUser"
-                println( "Received userName::: $userName")
-                val resultMap = mapOf("userName" to userName)
-                result.success(resultMap)
-            } else {
+                processUserName(call, userName, result)
+            } else if (call.method == ){
                 result.notImplemented()
             }
         }
