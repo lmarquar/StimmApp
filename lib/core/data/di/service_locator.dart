a6fe9b0e8c9d6f31d9b55d8ef1bcd5384846fa53
lmarquar <leon.marquardt@gmx.de>
2026-01-14 10:26:50 +0100

    hopefully we have a fully working state here

--- lib/core/data/di/service_locator.dart
+++ lib/core/data/di/service_locator.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
-import 'package:stimmapp/core/data/services/auth_service.dart';
+import 'package:stimmapp/core/data/services/auth_service.dart' as auth_glob;
 import 'package:stimmapp/core/data/services/database_service.dart';
 
 class ServiceLocator {
@@ -13,20 +13,20 @@ class ServiceLocator {
   late FirebaseFirestore _database;
   late DatabaseService _databaseService;
   late FirebaseAuth _auth;
-  late AuthService _authService;
+  late auth_glob.AuthService _authService;
 
   void init() {
     final app = Firebase.app();
     _database = FirebaseFirestore.instanceFor(app: app);
     _databaseService = DatabaseService(_database);
     _auth = FirebaseAuth.instanceFor(app: app);
-    _authService = authService;
+    _authService = auth_glob.authService;
   }
 
   FirebaseFirestore get database => _database;
   DatabaseService get databaseService => _databaseService;
   FirebaseAuth get auth => _auth;
-  AuthService get authService => _authService;
+  auth_glob.AuthService get authService => _authService;
 
   @visibleForTesting
   void setDatabaseForTest(FirebaseFirestore database) {
@@ -37,7 +37,7 @@ class ServiceLocator {
   @visibleForTesting
   void setAuthForTest(FirebaseAuth auth) {
     _auth = auth;
-    _authService = authService;
+    _authService = auth_glob.authService;
   }
 }
 
