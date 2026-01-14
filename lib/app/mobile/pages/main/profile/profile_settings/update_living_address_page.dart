6fbdf359743c4ad6c7bbf054b928a500b44442cd
lmarquar <leon.marquardt@gmx.de>
2026-01-14 14:33:37 +0100

    alot changed

--- lib/app/mobile/pages/main/profile/profile_settings/update_living_address_page.dart
+++ lib/app/mobile/pages/main/profile/profile_settings/update_living_address_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_places_flutter/google_places_flutter.dart';
+import 'package:google_places_flutter/model/place_type.dart';
+import 'package:google_places_flutter/model/prediction.dart';
 import 'package:stimmapp/app/mobile/scaffolds/app_bottom_bar_buttons.dart';
 import 'package:stimmapp/app/mobile/widgets/button_widget.dart';
 import 'package:stimmapp/app/mobile/widgets/select_address_widget.dart';
 import 'package:stimmapp/app/mobile/widgets/snackbar_utils.dart';
+import 'package:stimmapp/core/data/firebase/firebase_options.dart';
+import 'package:stimmapp/core/data/models/user_profile.dart';
 import 'package:stimmapp/core/data/repositories/user_repository.dart';
+import 'package:stimmapp/core/data/services/auth_service.dart';
 import 'package:stimmapp/core/extensions/context_extensions.dart';
-import 'package:stimmapp/core/functions/update_state.dart';
 import 'package:stimmapp/core/theme/app_text_styles.dart';
 
 class UpdateLivingAddressPage extends StatefulWidget {
@@ -19,6 +24,7 @@ class UpdateLivingAddressPage extends StatefulWidget {
 class _UpdateLivingAddressPageState extends State<UpdateLivingAddressPage>
     with WidgetsBindingObserver {
   String? _selectedState;
+  final TextEditingController _controllerAddress = TextEditingController();
   String errorMessage = '';
   final _formKey = GlobalKey<FormState>();
 
@@ -28,11 +34,18 @@ class _UpdateLivingAddressPageState extends State<UpdateLivingAddressPage>
     _loadInitialState();
   }
 
+  @override
+  void dispose() {
+    _controllerAddress.dispose();
+    super.dispose();
+  }
+
   Future<void> _loadInitialState() async {
     final userProfile = await UserRepository.currentUser();
     if (userProfile != null) {
       setState(() {
         _selectedState = userProfile.state;
+        _controllerAddress.text = userProfile.address ?? '';
       });
     }
   }
@@ -60,6 +73,49 @@ class _UpdateLivingAddressPageState extends State<UpdateLivingAddressPage>
                   child: Center(
                     child: Column(
                       children: [
+                        GooglePlaceAutoCompleteTextField(
+                          textEditingController: _controllerAddress,
+                          googleAPIKey:
+                              DefaultFirebaseOptions.currentPlatform.apiKey,
+                          inputDecoration: InputDecoration(
+                            labelText: context.l10n.address,
+                            border: const OutlineInputBorder(),
+                          ),
+                          countries: const ["de"],
+                          isLatLngRequired: false,
+                          placeType: PlaceType.address,
+                          getPlaceDetailWithLatLng: (prediction) {},
+                          debounceTime: 600,
+                          itemClick: (Prediction prediction) {
+                            _controllerAddress.text =
+                                prediction.description ?? "";
+                            _controllerAddress.selection =
+                                TextSelection.fromPosition(
+                                  TextPosition(
+                                    offset: prediction.description?.length ?? 0,
+                                  ),
+                                );
+                          },
+                          itemBuilder: (context, index, prediction) {
+                            return Container(
+                              padding: const EdgeInsets.all(10),
+                              child: Row(
+                                children: [
+                                  const Icon(Icons.location_on),
+                                  const SizedBox(width: 7),
+                                  Expanded(
+                                    child: Text(
+                                      prediction.description ?? "",
+                                      style: AppTextStyles.m,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                          seperatedBuilder: const Divider(),
+                        ),
+                        const SizedBox(height: 20),
                         SelectAddressWidget(
                           selectedState: _selectedState,
                           onStateChanged: (newValue) {
@@ -90,13 +146,22 @@ class _UpdateLivingAddressPageState extends State<UpdateLivingAddressPage>
           label: context.l10n.updateState,
           callback: () async {
             if (_formKey.currentState!.validate()) {
+              final successMessage = context.l10n.stateUpdatedSuccessfully;
               try {
-                updateState(_selectedState!);
+                final userRepository = UserRepository.create();
+                final uid = authService.currentUser!.uid;
+                final userProfile = await userRepository.getById(uid);
+                final updatedProfile = (userProfile ?? UserProfile(uid: uid))
+                    .copyWith(
+                      state: _selectedState,
+                      address: _controllerAddress.text,
+                    );
+                await userRepository.upsert(updatedProfile);
               } catch (e) {
                 showErrorSnackBar(e.toString());
                 return;
               }
-              showSuccessSnackBar(context.l10n.stateUpdatedSuccessfully);
+              showSuccessSnackBar(successMessage);
             }
           },
         ),
